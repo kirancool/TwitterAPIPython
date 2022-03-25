6660179b081dfcc00954aa5c4293c343288a3ccb
Kiran Adhav <kiran2adhav@gmail.com>
2022-03-25 13:12:39 +0530

    Add files via upload

--- TwitterAPIPython.py
+++ TwitterAPIPython.py
@@ -23,4 +23,37 @@ for tweet in data:
           'media_url':data['objects']['tweets']['504032379045179393']['entities']['media'][0]['media_url']})
   strdata=json.dumps(dff)
   df=pd.DataFrame(dff)
-  df
+  print(df)
+  
+*************************************************************************************************************************************************************************************
+
+
+#from urlib.request import urlopen
+import pandas as pd
+import json
+import requests
+from requests_oauthlib import OAuth1Session
+
+oauth_user = OAuth1Session(client_key='',
+                               client_secret='',
+                               resource_owner_key='',
+                               resource_owner_secret='')
+url_user = 'https://api.twitter.com/1.1/search/tweets.json?q=nasa&result_type=popular'
+r = oauth_user.get(url_user)
+data=r.json()
+#print(data)
+test=data['statuses'][0]
+dff=[]
+
+for tweet in range(100):
+    #print(tweet)
+    tweet={'created_at':test['created_at'],
+           'id':test['id'],
+           'url':test['entities']['urls'][0]['url'],
+           'expanded_url':test['entities']['urls'][0]['expanded_url'],
+          'user':test['user']['created_at'],
+          'name':test['user']['name']}
+    dff.append(tweet)
+    #strdata=json.dumps(df)
+d=pd.DataFrame(dff)
+d
